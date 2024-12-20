8abb6a06de69f3253fdb47a24a2d7e33cd9fc595
Drack27 <jdbusiness27@gmail.com>
2024-12-19 23:33:41 -0700

    protected route

--- frontend/readyupnroll-react-app/src/App.js
+++ frontend/readyupnroll-react-app/src/App.js
@@ -1,6 +1,6 @@
 import LandingPage from './LandingPage';
 import LoginPage from './LoginPage';
-import SignupPage from './SignUp'; 
+import SignupPage from './SignUp';
 import { Routes, Route } from 'react-router-dom';
 import HomeScreen from './HomeScreen';
 import SettingsScreen from './SettingsScreen';
@@ -15,27 +15,32 @@ import LeaveWorldConfirmation from './LeaveWorldConfirmation';
 import LeaveWorldSuccess from './LeaveWorldSuccess';
 import GroundRulesAcceptance from './GroundRulesAcceptance';
 import JoinWorldSuccess from './JoinWorldSuccess';
+import ProtectedRoute from './ProtectedRoute'; // Import your ProtectedRoute component
 
 function App() {
   return (
     <Routes>
+      {/* Public routes */}
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route path="/home" element={<HomeScreen />} />
-      <Route path="/settings" element={<SettingsScreen />} />
       <Route path="/AccountCreationSuccess" element={<AccountCreationSuccess />} />
-      <Route path="/WorldDetailsPlayer" element={<WorldDetailsPlayer />} />
-      <Route path="/WorldDetailsGM" element={<WorldDetailsGM />} />
-      <Route path="/JoinWorld" element={<JoinWorld />} />
-      <Route path="/InvitePlayers/:worldId" element={<InvitePlayers />} />
-      <Route path="/ReadyUpScreen" element={<ReadyUpScreen />} />
-      <Route path="/AvailabilitySubmitted" element={<AvailabilitySubmitted />} />
-      <Route path="/LeaveWorldConfirmation" element={<LeaveWorldConfirmation />} />
-      <Route path="/LeaveWorldSuccess" element={<LeaveWorldSuccess />} />
-      <Route path="/GroundRulesAcceptance" element={<GroundRulesAcceptance />} />
-      <Route path="/JoinWorldSuccess" element={<JoinWorldSuccess />} />
 
+      {/* Protected routes */}
+      <Route element={<ProtectedRoute />}> {/* Wrap protected routes */}
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/settings" element={<SettingsScreen />} />
+        <Route path="/WorldDetailsPlayer" element={<WorldDetailsPlayer />} />
+        <Route path="/WorldDetailsGM" element={<WorldDetailsGM />} />
+        <Route path="/JoinWorld" element={<JoinWorld />} />
+        <Route path="/InvitePlayers/:worldId" element={<InvitePlayers />} />
+        <Route path="/ReadyUpScreen" element={<ReadyUpScreen />} />
+        <Route path="/AvailabilitySubmitted" element={<AvailabilitySubmitted />} />
+        <Route path="/LeaveWorldConfirmation" element={<LeaveWorldConfirmation />} />
+        <Route path="/LeaveWorldSuccess" element={<LeaveWorldSuccess />} />
+        <Route path="/GroundRulesAcceptance" element={<GroundRulesAcceptance />} />
+        <Route path="/JoinWorldSuccess" element={<JoinWorldSuccess />} />
+      </Route>
     </Routes>
   );
 }
