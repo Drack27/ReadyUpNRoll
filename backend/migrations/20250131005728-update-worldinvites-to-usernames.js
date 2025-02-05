eff835d2864be9fd4935ee4e6cc1104e91530fcd
Drack27 <jdbusiness27@gmail.com>
2025-02-04 18:26:51 -0700

    DB table renaming

--- backend/migrations/20250131005728-update-worldinvites-to-usernames.js
+++ backend/migrations/20250131005728-update-worldinvites-to-usernames.js
@@ -1,53 +1,25 @@
-// migrations/[timestamp]-update-worldinvites-to-usernames.js
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Add the new username column
-    await queryInterface.addColumn('WorldInvites', 'username', {
-      type: Sequelize.STRING,
-      allowNull: true, // Temporarily allow null for data migration
-    });
+    // Rename the 'email' column to 'username'
+    await queryInterface.renameColumn('WorldInvites', 'email', 'username');
 
-    // Migrate data (populate username from Users table)
-    // This part might need adjustment based on your specific setup
-    const [invites] = await queryInterface.sequelize.query(
-      'SELECT * FROM "WorldInvites"'
-    );
-
-    for (const invite of invites) {
-      const [user] = await queryInterface.sequelize.query(
-        `SELECT username FROM "Users" WHERE email = '${invite.email}'` // Assuming you have an email field on Users
-      );
-
-      if (user.length > 0) {
-        await queryInterface.sequelize.query(
-          `UPDATE "WorldInvites" SET username = '${user[0].username}' WHERE id = ${invite.id}`
-        );
-      }
-    }
-
-    // Remove the old email column
-    await queryInterface.removeColumn('WorldInvites', 'email');
-
-    // Make username NOT NULL
+    // Change the column type if needed (it's already STRING in your original migration, so this might be unnecessary)
     await queryInterface.changeColumn('WorldInvites', 'username', {
       type: Sequelize.STRING,
-      allowNull: false,
+      allowNull: false, // Or true, depending on your requirements
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    // Add back the email column
-    await queryInterface.addColumn('WorldInvites', 'email', {
+    // Rename the 'username' column back to 'email'
+    await queryInterface.renameColumn('WorldInvites', 'username', 'email');
+
+    // Change the column type back if you changed it in 'up'
+    await queryInterface.changeColumn('WorldInvites', 'email', {
       type: Sequelize.STRING,
-      allowNull: true,
+      allowNull: true, // Or whatever it was originally
     });
-
-    // You might need to add logic to repopulate the email column (if possible)
-    // ...
-
-    // Remove the username column
-    await queryInterface.removeColumn('WorldInvites', 'username');
   },
 };
